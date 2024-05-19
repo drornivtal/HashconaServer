5f359849317a9d8cbce7898a5ed551f86d47d071
DrorCohen123 <158835481+drornivtal@users.noreply.github.com>
2024-05-19 15:21:59 +0300

    k

--- Hashchona/obj/Release/net6.0/Hashchona.AssemblyInfo.cs
+++ Hashchona/obj/Release/net6.0/Hashchona.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hashchona")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8001c4e2bf715fedd041f819bd8fab6a0ea1625")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hashchona")]
